641d4232c2021108b33fb37464bed6e62d16dc1f
Denis Malyarenko <mdn-fox@yandex.ru>
2022-08-09 00:30:49 +0500

    выполнил задачу 4

---
+++ задача4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Фиафан")
+{
+    Console.WriteLine("Фига какая встреча!");
+}
+else
+{
+    Console.Write("Здаровки, ");
+    Console.WriteLine(username);
+}
